a7646b4b0be56a9a11e2f0195e5175c09fbc713d
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-09-17 13:19:38 +0200

    Working on back end

---
+++ GeekQuiz/GeekQuiz/Controllers/TriviaController.cs
@@ -0,0 +1,44 @@
+﻿using GeekQuiz.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace GeekQuiz.Controllers
+{
+    public class TriviaController : ApiController
+    {
+        private TriviaContext db = new TriviaContext();
+
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing)
+            {
+                this.db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        private async Task<TriviaQuestion> NextQuestionAsync(string userId)
+        {
+            var lastQuestionId = await this.db.TriviaAnswers
+                .Where(a => a.UserId == userId)
+                .GroupBy(a => a.QuestionId)
+                .Select(g => new { QuestionId = g.Key, Count = g.Count() })
+                .OrderByDescending(q => new { q.Count, QuestionId = q.QuestionId })
+                .Select(q => q.QuestionId)
+                .FirstOrDefaultAsync();
+
+            var questionsCount = await this.db.TriviaQuestions.CountAsync();
+
+            var nextQuestionId = (lastQuestionId % questionsCount) + 1;
+            return await this.db.TriviaQuestions.FindAsync(CancellationToken.None, nextQuestionId);
+        }
+    }
+}
